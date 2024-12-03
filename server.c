9b3377066110886a546e27d4e1966f8a8fd04fcc
Ashley Brennan <161786991+ambrenn23@users.noreply.github.com>
2024-12-03 15:55:12 -0700

    Update server.c

--- server.c
+++ server.c
@@ -32,18 +32,13 @@ int main() {
 		// TODO:
 		// read requests from serverFIFO
 
-
-
-
-
+		if(read(server, &req, sizeof(struct message)) > 0){
+			target = open(req.target, O_WRONLY);
+			write(target, (void*)(&req), sizeof(struct message));
+			close(target);
+		}
 
 		printf("Received a request from %s to send the message %s to %s.\n",req.source,req.msg,req.target);
-
-		// TODO:
-		// open target FIFO and write the whole message struct to the target FIFO
-		// close target FIFO after writing the message
-
-
 
 
 
